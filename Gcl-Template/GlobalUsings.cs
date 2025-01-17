836377f1639700e6b455d6d51edd2b04622394e1
Gilian <giliankeulens@happyhorizon.com>
2025-01-17 16:54:38 +0100

    Upgrade to .NET 9

---
+++ Gcl-Template/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿// Global using directives
+global using System;
+global using System.Linq;
+global using System.Reflection;
+global using GeeksCoreLibrary.Core.Extensions;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
--- Gcl-Template/Program.cs
+++ Gcl-Template/Program.cs
@@ -1,16 +1,41 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Serilog;
 
-namespace Gcl_Template;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// First set the base settings for the application.
+builder.Configuration.AddJsonFile("appsettings.json", false, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true);
+
+// Get the base directory for secrets and then load the secrets file from that directory.
+var secretsBasePath = builder.Configuration.GetSection("GCL").GetValue<string>("SecretsBaseDirectory");
+if (!String.IsNullOrEmpty(secretsBasePath))
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args).Build().Run();
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    builder.Configuration
+        .AddJsonFile($"{secretsBasePath}appsettings-secrets.json", false, false)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true);
 }
+
+// Replace the placeholder with the project name.
+var logFileSection = builder.Configuration.GetSection("Serilog:WriteTo");
+var projectName = Assembly.GetEntryAssembly()?.GetName()?.Name ?? "Unknown";
+var sections = logFileSection.GetChildren().Where(section => (section["Args:path"] ?? "").Contains("{fileName}")).ToList();
+sections.ForEach(section => section["args:path"] = section["args:path"]!.Replace("{fileName}", $"{projectName}-{builder.Environment.EnvironmentName}-"));
+
+// Configure Serilog.
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+Log.Logger.Debug($"----- Project {projectName} has just been (re)started. -----");
+
+builder.Services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog());
+builder.Services.AddGclServices(builder.Configuration);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Add all GCL middlewares and other things the GCL needs,
+// such as controller endpoints, routing, static files middleware etc.
+app.UseGclMiddleware(builder.Environment);
+
+app.Run();
